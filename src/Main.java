ead2d64b5cf968ae7d27918a87ca0aab1bea6ec3
Cinthya León <162649790+CinthyaLeon@users.noreply.github.com>
2024-10-12 14:51:06 -0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner lector = new Scanner(System.in);
+        int numeroGenerado = new Random().nextInt(50);
+        int intentos = 0;
+
+        while (intentos < 3) {
+            System.out.println("introduce un número entre 0 y 50:");
+            int numeroDelUsuario = lector.nextInt();
+            intentos++;
+
+            if (numeroDelUsuario == numeroGenerado) {
+                System.out.println("genial, adivinaste");
+                break;
+            } else if (numeroDelUsuario > numeroGenerado) {
+                System.out.println("el número es menor");
+            } else {
+                System.out.println("el número es mayor");
+            }
+        }
+        if (intentos == 3) {
+            System.out.println("No adivinaste el numero en 3 intentos :(. El número es" + numeroGenerado);
+        }
     }
 }
